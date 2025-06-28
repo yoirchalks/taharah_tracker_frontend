aff5a66ad9c2883e00c8a5b027c3a4a43533f9f9
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-28 23:34:21 +0300

    added normalize email to async val

--- src/app/shared/validators/async.validators.ts
+++ src/app/shared/validators/async.validators.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { UsersService } from '../../auth/users.service';
 import { AbstractControl, AsyncValidatorFn } from '@angular/forms';
 import { catchError, debounceTime, map, of } from 'rxjs';
+import normalizeEmail from '../utils/normalizeEmail';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +11,9 @@ export class AsyncValidators {
   emailUniqueValidator = function (): AsyncValidatorFn {
     const usersService = inject(UsersService);
     return (control: AbstractControl) => {
-      const email = control.value;
+      let email = control.value;
       if (!email || !control.valid) return of(null);
+      email = normalizeEmail(email);
       return usersService.checkEmailUnique(email).pipe(
         map((isUnique) => (isUnique ? null : { emailTaken: true })),
         catchError(() => of({ emailCheckFailed: true }))
