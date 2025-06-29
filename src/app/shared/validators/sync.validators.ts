4d6afbec12afa6ad6403eca34c98af0ff1c097b7
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-29 19:37:11 +0300

    added modal

--- src/app/shared/validators/sync.validators.ts
+++ src/app/shared/validators/sync.validators.ts
@@ -3,8 +3,8 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export function passwordsMatch(
   group: AbstractControl
 ): ValidationErrors | null {
-  const password = group.get('password')?.value;
-  const repeatPassword = group.get('repeatPassword')?.value;
+  const password = group.get('password')?.value.trim();
+  const repeatPassword = group.get('repeatPassword')?.value.trim();
   if (!password || !repeatPassword) return null;
-  return password === repeatPassword ? null : { emailsDoNotMatch: true };
+  return password === repeatPassword ? null : { passwordsDoNotMatch: true };
 }
