6aea4675fe89f342fd664f01055b7bdcfdc32206
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-06-30 20:53:44 +0300

    added audio play functionality to button presses

--- src/app/auth/home/home.component.ts
+++ src/app/auth/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AudioService } from '../../shared/services/audio.service';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  audio = inject(AudioService);
+  isPlaying: boolean = false;
+  ngOnInit(): void {
+    this.isPlaying = this.audio.isPlaying();
+  }
+  onClick() {
+    if (!this.isPlaying) {
+      this.audio.play();
+    }
+  }
+}
