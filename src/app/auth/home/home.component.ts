10e3898ccc58d0d36d4dba136d4f6bd385a0abe1
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-14 17:59:05 +0300

    moved theme toggle to footer

--- src/app/auth/home/home.component.ts
+++ src/app/auth/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AudioService } from '../../shared/services/audio.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
