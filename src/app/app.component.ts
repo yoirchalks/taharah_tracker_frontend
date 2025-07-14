72bb9a10f975512f9fa5ecf13b84c70662526ddb
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-14 16:05:26 +0300

    toggle working. no functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { ThemeToggleComponent } from './home/nav-bar/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/home/nav-bar/theme-toggle/theme-toggle.component.ts
+++ src/app/home/nav-bar/theme-toggle/theme-toggle.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, output, Output } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-theme-toggle',
-  imports: [],
+  imports: [MatSlideToggleModule],
   templateUrl: './theme-toggle.component.html',
-  styleUrl: './theme-toggle.component.css'
+  styleUrl: './theme-toggle.component.css',
 })
 export class ThemeToggleComponent {
+  isDark = false;
+  themeChange = output<'dark' | 'light'>();
 
+  onToggle() {
+    this.isDark = !this.isDark;
+    this.themeChange.emit(this.isDark ? 'dark' : 'light');
+  }
 }
