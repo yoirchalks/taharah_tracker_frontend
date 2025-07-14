d3ac555272d507677f54628481b59a557a349bab
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-14 17:23:31 +0300

    added themese logic to app component. not working!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
-import { ThemeToggleComponent } from './home/nav-bar/theme-toggle/theme-toggle.component';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, ThemeToggleComponent],
+  imports: [RouterOutlet, FooterComponent, MatSlideToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'taharah-tracker';
+export class AppComponent implements OnInit {
+  title = 'Taharah Tracker';
+  isDark = false;
+
+  renderer = inject(Renderer2);
+
+  ngOnInit(): void {
+    this.renderer.addClass(document.body, 'mat-app-background');
+    this.renderer.addClass(document.body, 'light-theme');
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+
+    const newClass = this.isDark ? 'dark-theme' : 'light-theme';
+    const oldClass = !this.isDark ? 'dark-theme' : 'light-theme';
+
+    this.renderer.removeClass(document.body, oldClass);
+    this.renderer.addClass(document.body, newClass);
+  }
 }
