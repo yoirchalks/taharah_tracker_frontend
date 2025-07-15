5adfc1f4e819357d42b9a9aaf0d3fd3f95c60831
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-15 17:40:11 +0300

    develop landing page, paralax scrolling

--- src/app/auth/home/home.component.ts
+++ src/app/auth/home/home.component.ts
@@ -10,6 +10,20 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
+  texts = [
+    {
+      title: 'Welcome to a space of calm and security.',
+      text: `'3544011fd57b329b34b73c6cb08dc4a8f...' Do you know what this says?  We don't either. This is what your personal details look like after you enter them into our system. With industry level encryption, your personal data is guaranteed safe with us`,
+    },
+    {
+      title: 'Track with confidence and clarity.',
+      text: '',
+    },
+    {
+      title: 'Stay connected no matter where.',
+      text: '',
+    },
+  ];
   audio = inject(AudioService);
   isPlaying: boolean = false;
   ngOnInit(): void {
