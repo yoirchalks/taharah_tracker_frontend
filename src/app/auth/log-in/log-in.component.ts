73c5b7508ce750eb10b2ddeeefc0d2f7f2b45885
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-16 09:27:18 +0300

    set up form. looking good.

--- src/app/auth/log-in/log-in.component.ts
+++ src/app/auth/log-in/log-in.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrl: './log-in.component.css',
 })
 export class LogInComponent {
+  signInMethod = signal<'password' | 'otp'>('password');
 
+  form = new FormGroup({
+    email: new FormControl({}),
+    password: new FormControl({}),
+  });
+
+  get password() {
+    return this.form.controls.password;
+  }
+
+  get email() {
+    return this.form.controls.email;
+  }
+
+  onSubmit() {}
 }
