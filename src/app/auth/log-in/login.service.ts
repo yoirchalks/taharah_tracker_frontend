b128537f20f95e583fe82e92746c294149b78dfe
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-16 19:07:42 +0300

    added login service

---
+++ src/app/auth/log-in/login.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
+
+interface LogInData {
+  email: string;
+  password?: string;
+}
+
+interface OTPData {
+  userId: string;
+  id: string;
+  OTP: number;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  httpService = inject(HttpClient);
+  baseUrl = 'https://taharah-tracker-backend.onrender.com/api';
+
+  logIn = (data: LogInData) => {
+    this.httpService.post(`${this.baseUrl}/logIns`, data).pipe(
+      catchError((err) => {
+        return throwError(() => {
+          new Error(
+            err.error.message || err.message || 'log in attempt failed'
+          );
+        });
+      })
+    );
+  };
+
+  submitOtp = (data: OTPData) => {
+    this.httpService.post(`${this.baseUrl}/otp`, data).pipe(
+      catchError((err) => {
+        return throwError(() => {
+          new Error(
+            err.error.message || err.message || 'log in attempt failed'
+          );
+        });
+      })
+    );
+  };
+
+  constructor() {}
+}
