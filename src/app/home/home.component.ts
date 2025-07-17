61c1093250ecc4567f117fef251c2ae789aa779e
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-17 22:32:40 +0300

    added router outlet to home home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
