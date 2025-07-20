ce2adbcd94261ec2fe14e40df2cb0482121970f9
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-20 22:30:35 +0300

    moved guard file

---
+++ src/app/shared/guards/canMatch.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanDeactivate,
+  CanMatch,
+  GuardResult,
+  MaybeAsync,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree,
+} from '@angular/router';
+import { catchError, map, Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class authGuard implements CanMatch {
+  httpService = inject(HttpClient);
+  router = inject(Router);
+
+  canMatch(
+    route: Route,
+    segments: UrlSegment[]
+  ): Observable<boolean | UrlTree> {
+    return this.httpService
+      .get('https://taharah-tracker-backend.onrender.com/api/authChack', {
+        withCredentials: true,
+      })
+      .pipe(
+        map(() => true),
+        catchError(() => of(this.router.parseUrl('/auth/home')))
+      );
+  }
+
+  canDeactivate() {
+    return true;
+  }
+}
