07c707e6dbefe6f6e0e8457b79fb431c2f0af0ec
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-21 16:33:14 +0300

    added post periods. changed model location

--- src/app/shared/services/hebrew-date.service.ts
+++ src/app/shared/services/hebrew-date.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
-import ZmanimData from './zmanim.model';
+import ZmanimData from '../models/zmanim.model';
 
 @Injectable({
   providedIn: 'root',
--- src/app/shared/services/periods.service.ts
+++ src/app/shared/services/periods.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 
 @Injectable({
@@ -11,6 +11,10 @@ export class PeriodsService {
   httpService = inject(HttpClient);
   url = 'https://taharah-tracker-backend.onrender.com/api/periods';
 
+  postPeriod(type: string, date: string) {
+    this.httpService.post(this.url, { periodType: type, dateTime: date });
+  }
+
   getPeriods(
     userId: any,
     startDate: string,
@@ -18,10 +22,14 @@ export class PeriodsService {
     limit: number,
     page: number
   ) {
+    const httpParams = new HttpParams();
+    if (startDate) httpParams.set('startDate', startDate);
+    if (endDate) httpParams.set('endDate', endDate);
+    if (limit != null) httpParams.set('limit', limit);
+    if (page != null) httpParams.set('page', page);
+
     this.httpService
-      .get<any[]>(
-        `${this.url}/:${userId}?startDate=${startDate}endDate=${endDate}limit=${limit}page=${page}`
-      )
+      .get<any[]>(`${this.url}/${userId}`, { params: httpParams })
       .subscribe({
         next: (periods) => this.periods$.set(periods),
         error: (err) => alert(err),
