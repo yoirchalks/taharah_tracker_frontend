861629348a727f5b803d2e20a8caacc600593c62
yoirchalk1995 <yoirchalk1995@gmail.com>
2025-07-23 16:32:35 +0300

    created clickOut directive

---
+++ src/app/shared/click-out.directive.ts
@@ -0,0 +1,22 @@
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  inject,
+  output,
+} from '@angular/core';
+
+@Directive({
+  selector: '[clickOut]',
+})
+export class ClickOutDirective {
+  clickOut = output<Event>();
+  elementRef = inject(ElementRef);
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.clickOut.emit(event);
+    }
+  }
+}
